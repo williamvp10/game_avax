c95fd09ab4102cbe78ac750deac16b8db8946907
William Vasquez <williamdvp@hotmail.com>
2024-09-02 01:49:51 -0500

    Despligue de contratos en testnet

--- frontend_hub/packages/hardhat/deploy/00_deploy_your_contract.ts
+++ frontend_hub/packages/hardhat/deploy/00_deploy_your_contract.ts
@@ -32,7 +32,8 @@ const deployContracts: DeployFunction = async function (hre: HardhatRuntimeEnvir
   const { deploy } = hre.deployments;
   const { ethers, network } = hre;
   console.log("Deploying contracts with account:", deployer, network.name);
-  const networkConfig = axelarNetworks[network.name];
+  let network_conf = network.name=== "localhost" ? network.name: "testnet" ;
+  const networkConfig = axelarNetworks[network_conf];
 
   // Variables comunes
   const feePercentage = 2;
@@ -108,7 +109,7 @@ const deployContracts: DeployFunction = async function (hre: HardhatRuntimeEnvir
 
     const nftCollectionDeployment = await deploy("CrossChainNFTCollection", {
       from: deployer,
-      args: ["Sea of Fortune Ship", "SFS", deployer, "https://metadata-uri.com/", gatewayAddress, gasServiceAddress],
+      args: ["Sea of Fortune Ship", "SFS", deployer, "https://ipfs.io/ipfs/QmP4CkvwLGjT8qJqCZzGjkS6bczBfbPLWTXwNsdtbTyCjR/{id}.json", gatewayAddress, gasServiceAddress],
       log: true,
       autoMine: true,
     });
@@ -161,7 +162,7 @@ const deployContracts: DeployFunction = async function (hre: HardhatRuntimeEnvir
 
     const nftCollectionDeployment = await deploy("CrossChainNFTCollection", {
       from: deployer,
-      args: ["Sea of Fortune Ship", "SFS", deployer, "https://metadata-uri.com/", gatewayAddress, gasServiceAddress],
+      args: ["Sea of Fortune Ship", "SFS", deployer, "https://ipfs.io/ipfs/QmP4CkvwLGjT8qJqCZzGjkS6bczBfbPLWTXwNsdtbTyCjR/{id}.json", gatewayAddress, gasServiceAddress],
       log: true,
       autoMine: true,
     });
@@ -190,39 +191,41 @@ const deployContracts: DeployFunction = async function (hre: HardhatRuntimeEnvir
     console.log("SeaFortune deployed at:", seaFortuneAddress);
 
     // Configurar direcciones de confianza para contratos en Avalanche Fuji y Sepolia
+    /*
     const crossChainNFTCollection = await ethers.getContract<Contract>("CrossChainNFTCollection", deployer);
-    await crossChainNFTCollection.setTrustedRemote(networkConfig["ethereum_Sepolia"].axelarName, sepoliaDeployments.nftCollectionAddress);
-    await crossChainNFTCollection.setTrustedRemote(networkConfig["avalanche_fuji"].axelarName, nftCollectionAddress);
+    await crossChainNFTCollection.setTrustedRemote(networkConfig["ethereum_Sepolia"]["axelarName"], sepoliaDeployments.nftCollectionAddress);
+    await crossChainNFTCollection.setTrustedRemote(networkConfig["avalanche_fuji"]["axelarName"], nftCollectionAddress);
     console.log("Trusted remotes set for CrossChainNFTCollection on Avalanche Fuji");
 
     const seaFortune = await ethers.getContract<Contract>("SeaFortune", deployer);
-    await seaFortune.setTrustedRemote(networkConfig["ethereum_Sepolia"].axelarName, sepoliaDeployments.seaFortuneAddress);
-    await seaFortune.setTrustedRemote(networkConfig["avalanche_fuji"].axelarName, seaFortuneAddress);
+    await seaFortune.setTrustedRemote(networkConfig["ethereum_Sepolia"]["axelarName"], sepoliaDeployments.seaFortuneAddress);
+    await seaFortune.setTrustedRemote(networkConfig["avalanche_fuji"]["axelarName"], seaFortuneAddress);
     console.log("Trusted remotes set for SeaFortune on Avalanche Fuji");
 
     // Configurar el crossChainToken
     const crossChainToken = await ethers.getContract<Contract>("CrossChainToken", deployer);
-    await crossChainToken.setTrustedRemote(networkConfig["ethereum_Sepolia"].axelarName, sepoliaDeployments.tokenAddress);
-    await crossChainToken.setTrustedRemote(networkConfig["avalanche_fuji"].axelarName, tokenAddress);
+    await crossChainToken.setTrustedRemote(networkConfig["ethereum_Sepolia"]["axelarName"], sepoliaDeployments.tokenAddress);
+    await crossChainToken.setTrustedRemote(networkConfig["avalanche_fuji"]["axelarName"], tokenAddress);
     console.log("Trusted remote set for CrossChainToken on Avalanche Fuji");
-
+    */
   }
 
   // Paso 3: Configurar Sepolia después de Avalanche
   if (network.name === "ethereum_Sepolia") {
+    /*
     const crossChainNFTCollection = await ethers.getContract<Contract>("CrossChainNFTCollection", deployer);
-    await crossChainNFTCollection.setTrustedRemote(networkConfig["avalanche_fuji"].axelarName, sepoliaDeployments.nftCollectionAddress);
+    await crossChainNFTCollection.setTrustedRemote(networkConfig["avalanche_fuji"]["axelarName"], sepoliaDeployments.nftCollectionAddress);
     console.log("Trusted remote set for CrossChainNFTCollection on Sepolia");
 
     const seaFortune = await ethers.getContract<Contract>("SeaFortune", deployer);
-    await seaFortune.setTrustedRemote(networkConfig["avalanche_fuji"].axelarName, sepoliaDeployments.seaFortuneAddress);
+    await seaFortune.setTrustedRemote(networkConfig["avalanche_fuji"]["axelarName"], sepoliaDeployments.seaFortuneAddress);
     console.log("Trusted remote set for SeaFortune on Sepolia");
 
     // Configurar el crossChainToken
     const crossChainToken = await ethers.getContract<Contract>("CrossChainToken", deployer);
-    await crossChainToken.setTrustedRemote(networkConfig["avalanche_fuji"].axelarName, sepoliaDeployments.tokenAddress);
+    await crossChainToken.setTrustedRemote(networkConfig["avalanche_fuji"]["axelarName"], sepoliaDeployments.tokenAddress);
     console.log("Trusted remote set for CrossChainToken on Sepolia");
-
+    */
   }
 };
 
